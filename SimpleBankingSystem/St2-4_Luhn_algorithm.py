ff875853972353ad7bacebef3f74678ef7189019
konovalovMV <63033292+konovalovMV@users.noreply.github.com>
2020-07-27 11:21:49 +0500

    Update St2-4_Luhn_algorithm.py

--- SimpleBankingSystem/St2-4_Luhn_algorithm.py
+++ SimpleBankingSystem/St2-4_Luhn_algorithm.py
@@ -3,7 +3,7 @@
 #вычесть 9 в числа больше 9
 #добавить все номера
 
-card_number = '400000844943340'
+card_number = '400000997268569'
 
 def LUH(card_number):
     card = [int(num) for num in card_number]
@@ -18,6 +18,9 @@ def LUH(card_number):
         if tmp > 9:
             tmp -= 9
         sum +=tmp
-    return str(10 - (sum % 10))
+    if sum % 10 == 0:
+        return '0'
+    else:
+        return str(10 - (sum % 10))
 
 print(LUH(card_number))
